56148ede1e3a2e76729d9bc527d9b744696db7f1
stevennt <32351181+stevennt@users.noreply.github.com>
2022-11-04 09:45:26 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# abn_video_player
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-player-demo-nsozjn)
---
+++ index.tsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import ReactPlayer from 'react-player';
+import './style.css';
+
+
+const params = new URLSearchParams(window.location.search);
+//const paramValue = "https://res.cloudinary.com/wikacy-com/video/upload/v1642352266/trailers/zeitgeist-trailer_njhtpf.mp4"
+
+const paramValue = params.get("url");
+
+
+ 
+
+const App: React.FC = () => (
+  <div>
+    <ReactPlayer url={paramValue}
+    
+    playing     
+    controls
+    preload
+    disableRemotePlayback={true}
+    allowFullscreen ={false}
+    playbackrate= {false}
+    config={{ file: { attributes: { controlsList: 'nodownload noplaybackrate nofullscreen noremoteplayback',disablePictureInPicture: true } } }}
+    width='100%'
+    height='100%'
+    light
+     />
+  </div>
+);
+
+render(<App />, document.getElementById('root'));
